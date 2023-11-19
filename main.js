e3822094842c47b164460274d1c090d2463cc8e4
Savenko Ivan PC <savenkoivan931@gmail.com>
2023-11-19 13:02:50 +0100

    LC1

--- main.js
+++ main.js
@@ -6,21 +6,9 @@ const scene = new THREE.Scene();
 
 const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
 const material = new THREE.MeshNormalMaterial();
-
+let x =5
 const mesh = new THREE.Mesh( geometry, material );
 scene.add( mesh );
-const renderer = new THREE.WebGLRenderer( { antialias: true } );
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize( width, height );
-renderer.setAnimationLoop( animation );
 document.body.appendChild( renderer.domElement );
-
-// animation
-
-function animation( time ) {
-
-	mesh.rotation.x = time / 2000;
-	mesh.rotation.y = time / 1000;
-
-	renderer.render( scene, camera );
-
-}
